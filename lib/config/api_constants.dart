1c78a9489e4db1051b16fc0faf72fc0ee943cea8
Sharvin R <sharvinramesh1@gmail.com>
2026-03-20 16:39:19 +0530

    "commit customer support chat api integration"

--- lib/config/api_constants.dart
+++ lib/config/api_constants.dart
@@ -12,6 +12,10 @@ class ApiConstants {
   static const String profile = '${baseUrl}profile';
   static const String updateprofile = '${baseUrl}profile/update';
 
+  // Chat
+  static const String getMessages = '${baseUrl}messages';
+  static const String sendMessage = '${baseUrl}messages/send';
+
 
   static const String sendOtp = '${baseUrl}forgot_password/send_otp';
   static const String verifyOtp = '${baseUrl}forgot_password/verify_otp';
