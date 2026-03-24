47bcf94dead21ec5b2b51d5a0666ce61dad990be
Sharvin R <sharvinramesh1@gmail.com>
2026-03-24 18:01:34 +0530

    "missing import added"

--- lib/view/settings/maintenance.dart
+++ lib/view/settings/maintenance.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../../generated/l10n.dart';
 class Maintenance extends StatelessWidget {
   const Maintenance({super.key, required this.serverDownReason});
   final String? serverDownReason;
